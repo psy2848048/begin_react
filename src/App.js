5ddc16c0f4e1ebfe77bfa44b8bd10b65d761a640
Bryan Rhee <psy2848048@gmail.com>
2022-06-20 18:42:24 +0900

    feat: studied to useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import Counter from './Counter';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
+import InputSample from './InputSample';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true}/>
-      <Hello color="black" />
-    </Wrapper>
+    <InputSample />
   );
 }
 
