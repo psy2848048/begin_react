2e13d5750b532661b170aaeb5da1f76091f244a1
Bryan Rhee <psy2848048@gmail.com>
2022-06-21 15:28:39 +0900

    feat: tested useMemo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,84 @@
+import React, {useState, useRef, useMemo, useCallback} from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const { username, email } = inputs;
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    },
+    [inputs]
+  );
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = useCallback( () => {
+    const user = {
+      id: nextId.current,
+      username: username, 
+      email: email
+    };
+
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+
+    nextId.current += 1;
+  }, [users, username, email]);
+
+  const onRemove = useCallback(id => {
+    setUsers(users.filter(user => user.id !== id));
+  }, [users]);
+
+  const onToggle = id => {
+    setUsers(users.map(user => user.id === id ? {...user, active: !user.active}: user));
+  };
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <>
-      <UserList />
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자수: {count}</div>
     </>
   );
 }
